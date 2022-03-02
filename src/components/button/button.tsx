b8804ab5a788d905b5a2d6bbada07fe1ff4472f5
Gregoria  Millensifer <gregoriiaaa@Gregorias-MBP.lan>
2022-03-02 10:59:48 -0800

    Finished

---
+++ src/components/button/button.tsx
@@ -0,0 +1,52 @@
+import React, { MouseEvent } from "react";
+import "./button.css";
+
+// HINT: Use this in ButtonProps to determine how the button is styled
+export enum ButtonType {
+  "button-green",
+  "button-white",
+  "button-clear",
+  "button-clearAlt",
+}
+
+// HINT: Use this in ButtonProps to determine how the button text is styled
+export enum ButtonTextType {
+  "button-text-white",
+  "button-text-green",
+  "button-text-grey",
+}
+
+interface ButtonProps {
+  text: string;
+  buttonType: string;
+  textType: string;
+  children?: React.ReactNode;
+  onPress?: any;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  buttonType,
+  textType,
+  children,
+  onPress,
+}) => {
+  const clickHandler = (
+    // When passing a union as a type parameter,
+    // you can provide type-safety to a more
+    // abstract handlers:
+    e: MouseEvent<HTMLButtonElement | HTMLAnchorElement>
+  ): void => {
+    e.preventDefault();
+    onPress();
+  }
+  return (
+    <button onClick={clickHandler} className="App-button" id={buttonType}>
+      <p className="App-button-text" id={textType}>
+        {text}
+      </p>
+    </button>
+  );
+};
+
+export default Button;
